7de99d1f2cffa8b6a2c2956b83d7f880a90928a1
BrunoSouzaPicinini <brunosouzapicinini@gmail.com>
2019-03-31 01:02:46 -0300

    commit initial

---
+++ src/main/java/br/com/casadocodigo/loja/infra/FileSaver.java
@@ -0,0 +1,39 @@
+package br.com.casadocodigo.loja.infra;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Component
+public class FileSaver {
+
+    @Autowired
+    private AmazonS3 amazonS3;
+    private static final String BUCKET = "casadocodigobspicinini";
+
+    public String write(MultipartFile file) {
+        try {
+            amazonS3.putObject(new PutObjectRequest(BUCKET, file.getOriginalFilename(), file.getInputStream(), null)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+
+            return "http://s3.amazonaws.com/" + BUCKET + "/" + file.getOriginalFilename();
+        } catch (IllegalStateException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
